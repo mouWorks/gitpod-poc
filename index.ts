dd4a20c54c1b1d7e827fdd22aac7f96e64fe2d72
MWang <mou.wang@g.ncu.edu.tw>
2021-08-01 19:00:31 +0000

    working

--- index.ts
+++ index.ts
@@ -1,7 +1,34 @@
 import { someString } from './utils'
+import { Knex, knex } from 'knex'
 const words = someString()
 console.log(words)
 
 export function say() {
   return words
+}
+
+export async function checkDB() {
+  interface User {
+    id: number
+    name: string
+  }
+
+  const config: Knex.Config = {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      port: 5430,
+      user: 'monkey_user',
+      password: 'monkey_pass',
+      database: 'monkey_db'
+    }
+  }
+
+  const knexInstance = knex(config)
+
+  try {
+    const users = await knex<User>('users').select('id', 'name')
+  } catch (err) {
+    // error handling
+  }
 }
